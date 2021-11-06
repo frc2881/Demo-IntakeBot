3826acd956ab17efa781e38f670f20b492ffb99a
frc2881 <frc2881@gmail.com>
2021-11-05 19:52:52 -0500

    Drive

--- BasicCode/src/main/java/frc/robot/RobotContainer.java
+++ BasicCode/src/main/java/frc/robot/RobotContainer.java
@@ -10,39 +10,78 @@ import frc.robot.commands.ExampleCommand;
 import frc.robot.subsystems.ExampleSubsystem;
 import edu.wpi.first.wpilibj2.command.Command;
 
-/**
- * This class is where the bulk of the robot should be declared. Since Command-based is a
- * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
- * periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
- * subsystems, commands, and button mappings) should be declared here.
- */
+
 public class RobotContainer {
   // The robot's subsystems and commands are defined here...
-  private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
+  XboxController driverController = new XboxController(0);
+  XboxController manipulatorController = new XboxController(1);
 
-  private final ExampleCommand m_autoCommand = new ExampleCommand(m_exampleSubsystem);
+  private final Drive drive = new Drive();
 
-  /** The container for the robot. Contains subsystems, OI devices, and commands. */
+  private final DriveWithJoysticks driveWithJoysticks = new DriveWithJoysticks(
+    drive, 
+    () -> getDriverLeftY(),
+    () -> -getDriverRightX());
+
   public RobotContainer() {
     // Configure the button bindings
     configureButtonBindings();
+
+    drive.setDefaultCommand(driveWithJoysticks);
   }
 
-  /**
-   * Use this method to define your button->command mappings. Buttons can be created by
-   * instantiating a {@link GenericHID} or one of its subclasses ({@link
-   * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing it to a {@link
-   * edu.wpi.first.wpilibj2.command.button.JoystickButton}.
-   */
-  private void configureButtonBindings() {}
+  private void configureButtonBindings() {
+    new JoystickButton(driverController, Button.kBumperRight.value)
+            .whenPressed(() -> drive.setMaxOutput(0.5))
+            .whenReleased(() -> drive.setMaxOutput(1));
+  }
 
-  /**
-   * Use this to pass the autonomous command to the main {@link Robot} class.
-   *
-   * @return the command to run in autonomous
-   */
-  public Command getAutonomousCommand() {
-    // An ExampleCommand will run in autonomous
-    return m_autoCommand;
+  private double getDriverLeftX() {
+    return(driverController.getRawAxis(0));
+  }
+
+private double getDriverLeftY() {
+    return(driverController.getRawAxis(1));
+  }
+
+private double getDriverRightX() {
+    return(driverController.getRawAxis(2));
+  }
+
+private double getDriverRightY() {
+    return(driverController.getRawAxis(5));
+  }
+
+private double getDriverLeftTrigger() {
+    return((driverController.getRawAxis(3) + 1) / 2);
+  }
+
+private double getDriverRightTrigger() {
+    return((driverController.getRawAxis(4) + 1) / 2);
+  }
+
+private double getManipulatorLeftX() {
+    return(manipulatorController.getRawAxis(0));
+  }
+
+private double getManipulatorLeftY() {
+    return(manipulatorController.getRawAxis(1));
+  }
+
+private double getManipulatorRightX() {
+    return(manipulatorController.getRawAxis(2));
+  }
+
+private double getManipulatorRightY() {
+    return(manipulatorController.getRawAxis(5));
+  }
+
+private double getManipulatorLeftTrigger() {
+    return((manipulatorController.getRawAxis(3) + 1) / 2);
+  }
+
+private double getManipulatorRightTrigger() {
+    return((manipulatorController.getRawAxis(4) + 1) / 2);
   }
 }
+
